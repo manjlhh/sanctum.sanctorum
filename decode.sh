299d18fd1bf7c66c86a4421ac88caf0f957e8bd2
manjlhh <man@jl.hh>
2023-03-31 12:30:11 +0400

    master

---
+++ decode.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env sh
+
+COLORS="true"
+enable_colors() {
+    ALL_OFF="\e[1;0m"
+    BOLD="\e[1;1m"
+    GREEN="${BOLD}\e[1;32m"
+    BLUE="${BOLD}\e[1;34m"
+
+    PACMAN_COLORS='--color=always'
+    PACCACHE_COLORS=''
+    MAKEPKG_COLORS=''
+}
+info() {
+    local mesg=$1; shift
+    printf "${BLUE}  ->${ALL_OFF}${BOLD} ${mesg}${ALL_OFF}\n" "$@"
+}
+error() {
+    local mesg=$1; shift
+    printf "${RED}==> ERROR:${ALL_OFF}${BOLD} ${mesg}${ALL_OFF}\n" "$@"
+}
+msg() {
+    local mesg=$1; shift
+    printf "\n${GREEN}==>${ALL_OFF}${BOLD} ${mesg}${ALL_OFF}\n" "$@"
+}
+[ "$COLORS" = "true" ] && enable_colors
+
+
+
+if [ $# -eq 0 ]; then
+	error 'password required'
+	exit -1
+fi
+
+HASH=$(printf '%s' "$1" | shasum -a 512 | awk '{ print $1 }')
+HASH=${HASH:0:64}
+if [ "$HASH" != 'abf9c11b33a42bc576694f7ec0b891e1f780c12811f4032018ba9b5f98dfee15' ]; then
+	error "wrong password"
+	exit -1
+fi
+
+WORKDIR=$(dirname "$(readlink -f "$0")")
+TARGET="$WORKDIR/dec/sanctum.sanctorum.kdbx"
+TARGETDIR=$(dirname "$TARGET")
+ENC_FILE="$WORKDIR/sanctum.sanctorum.enc"
+
+if [ ! -f "$ENC_FILE" ]; then
+	error "no file $ENC_FILE"
+	exit -1
+fi
+
+mkdir -p "$TARGETDIR"
+
+msg 'decrypting'
+openssl aes-256-cbc -d -pass "pass:$1" -salt -pbkdf2 -in "$ENC_FILE" -out "$TARGET"
